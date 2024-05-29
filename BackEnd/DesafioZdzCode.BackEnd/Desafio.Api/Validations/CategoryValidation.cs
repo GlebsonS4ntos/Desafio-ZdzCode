2b1b20e72aa56df8bea4e5016a4072cad6c25ce9
Glebson Santos <glebsonsantos@outlook.pt>
2024-05-29 10:22:27 -0300

    Validacoes com FluentValidation

---
+++ BackEnd/DesafioZdzCode.BackEnd/Desafio.Api/Validations/CategoryValidation.cs
@@ -0,0 +1,16 @@
+﻿using Desafio.Api.Dtos.Category;
+using FluentValidation;
+
+namespace Desafio.Api.Validations
+{
+    public class CategoryValidation : AbstractValidator<CategoryDto>
+    {
+        public CategoryValidation() 
+        { 
+            RuleFor(c => c.Name).NotNull().WithMessage("O Nome da Categoria n pode ser nulo.");
+
+            RuleFor(c => c.Name).NotEmpty().When(c => c.Name != null).WithMessage("O Nome da Categoria n pode ser vazio.")
+                .MinimumLength(2).WithMessage("O Nome da Categoria deve ter no minimo 2 caracteres.");
+        }
+    }
+}
---
+++ BackEnd/DesafioZdzCode.BackEnd/Desafio.Api/Validations/ProductValidation.cs
@@ -0,0 +1,34 @@
+﻿using Desafio.Api.Dtos.Product;
+using FluentValidation;
+
+namespace Desafio.Api.Validations
+{
+    public class ProductValidation : AbstractValidator<ProductDto>
+    {
+        public ProductValidation() 
+        {
+            RuleFor(p => p.Name).NotNull().WithMessage("O Nome do Produto n pode ser nulo.");
+
+            RuleFor(p => p.Name).NotEmpty().When(p => p.Name != null).WithMessage("O Nome do Produto n pode ser vazio.")
+                .MinimumLength(2).WithMessage("O Nome do Produto deve ter no minimo 2 caracteres.");
+
+            RuleFor(p => p.Description).NotNull().WithMessage("A Descrição do Produto n pode ser nulo.");
+
+            RuleFor(p => p.Description).NotEmpty().When(p => p.Description != null).WithMessage("A Descrição do Produto n pode ser vazio.")
+                .MinimumLength(5).WithMessage("A Descrição do Produto deve ter no minimo 5 caracteres.");
+
+            RuleFor(p => p.Price).GreaterThan(0.0m).WithMessage("O Preço do Produto deve ser maior que 0");
+
+            RuleFor(p => p.ImageUrl).NotNull().WithMessage("O Produto deve conter uma Imagem.");
+
+            RuleFor(p => p.ImageUrl).NotEmpty().When(p => p.ImageUrl != null).WithMessage("O Produto deve conter uma Imagem.")
+                .MinimumLength(10).WithMessage("O Produto deve conter uma Imagem.");
+
+            RuleFor(p => p.CategoryId).NotNull().WithMessage("O Produto precisa conter uma Categoria.");
+
+            RuleFor(p => p.CategoryId).NotEmpty().When(p => p.CategoryId != null).WithMessage("O Produto precisa conter uma Categoria.");
+
+            RuleFor(p => p.Quantity).GreaterThanOrEqualTo(0).WithMessage("O Produto deve ter 0 ou mais itens.");
+        }
+    }
+}
