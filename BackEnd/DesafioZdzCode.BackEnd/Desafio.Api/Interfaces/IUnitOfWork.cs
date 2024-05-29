5be186fc740812e576f81c606f2be4e6d46a7f25
Glebson Santos <glebsonsantos@outlook.pt>
2024-05-29 06:39:53 -0300

    Padrao UnitOfWork

---
+++ BackEnd/DesafioZdzCode.BackEnd/Desafio.Api/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Desafio.Api.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepositoryCategory RepositoryCategory { get; }
+        IRepositoryProduct RepositoryProduct { get; }
+
+        Task Commit();
+    }
+}
---
+++ BackEnd/DesafioZdzCode.BackEnd/Desafio.Api/Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using Desafio.Api.Data;
+using Desafio.Api.Interfaces;
+
+namespace Desafio.Api.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DesafioContext _context;
+
+        private IRepositoryCategory? categoryRepository;
+        private IRepositoryProduct? productRepository;
+
+        public UnitOfWork(DesafioContext context)
+        {
+            _context = context;
+        }
+
+        public IRepositoryCategory RepositoryCategory 
+        {
+            get 
+            { 
+                return categoryRepository = categoryRepository ?? new RepositoryCategory(_context); 
+            } 
+        }
+
+        public IRepositoryProduct RepositoryProduct 
+        {
+            get 
+            {  
+                return productRepository = productRepository ?? new RepositoryProduct(_context); 
+            } 
+        }
+
+        public async Task Commit()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
