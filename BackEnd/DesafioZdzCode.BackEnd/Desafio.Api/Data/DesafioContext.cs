754483b5ba102bfde68fd6f48d991a2bcd91f867
Glebson Santos <glebsonsantos@outlook.pt>
2024-05-29 04:40:34 -0300

    Adicionando DbContext

---
+++ BackEnd/DesafioZdzCode.BackEnd/Desafio.Api/Data/DesafioContext.cs
@@ -0,0 +1,31 @@
+﻿using Desafio.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio.Api.Data
+{
+    public class DesafioContext : DbContext
+    {
+        public DesafioContext(DbContextOptions<DesafioContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(p => p.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Category>()
+                .HasOne(c => c.ParentCategory)
+                .WithMany()
+                .HasForeignKey(c => c.PartentCategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- BackEnd/DesafioZdzCode.BackEnd/Desafio.Api/Program.cs
+++ BackEnd/DesafioZdzCode.BackEnd/Desafio.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using Desafio.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Desafio.Api
 {
     public class Program
@@ -13,6 +16,13 @@ namespace Desafio.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            var connectionString = builder.Configuration.GetConnectionString("Sqlite");
+
+            builder.Services.AddDbContext<DesafioContext>(opt =>
+            {
+                opt.UseSqlite(connectionString);
+            });
 
             var app = builder.Build();
 
