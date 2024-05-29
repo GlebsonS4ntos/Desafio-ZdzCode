e505d45a8658aacadc273bca6bb95f9502fc9727
Glebson Santos <glebsonsantos@outlook.pt>
2024-05-29 07:39:19 -0300

    Dtos

---
+++ BackEnd/DesafioZdzCode.BackEnd/Desafio.Api/Dtos/Category/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Desafio.Api.Dtos.Category
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid PartentCategoryId { get; set; }
+    }
+}
