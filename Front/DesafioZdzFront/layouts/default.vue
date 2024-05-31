a8e2c906e3fb3eeca80c8c2bc3512ae03f2befed
Glebson Santos <glebsonsantos@outlook.pt>
2024-05-31 16:56:31 -0300

    Adicionando layout com NavBar

---
+++ Front/DesafioZdzFront/layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-app dark="true">
+        <v-navigation-drawer v-model="drawer" :clipped="clipped" temporary :location="bottom">
+            <v-list>
+                <v-list-item v-for="(item, i) in items" :key="i" :value="item" color="primary" :to="item.to" exact>
+                    <template v-slot:prepend>
+                        <v-icon :icon="item.icon"></v-icon>
+                    </template>
+                    <v-list-item-title v-text="item.title"></v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar :clipped-left="clipped" fixed app color="purple-darken-2">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <v-toolbar-title @click.prevent="goHome">
+                {{ title }}
+            </v-toolbar-title>
+            <v-spacer />
+        </v-app-bar>
+        <v-main>
+            <v-container>
+                <slot />
+            </v-container>
+        </v-main>
+        <v-footer :absolute="!fixed" app>
+            <span>&copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'DefaultLayout',
+    data() {
+        return {
+            clipped: false,
+            drawer: false,
+            fixed: true,
+            items: [
+                {
+                    icon: 'mdi-ballot-outline',
+                    title: 'Categorias',
+                    to: '/categorias'
+                },
+                {
+                    icon: 'mdi-train-car-container',
+                    title: 'Produtos',
+                    to: '/produtos'
+                }
+            ],
+            miniVariant: false,
+            title: 'Controle de Estoque'
+        }
+    },
+    methods: {
+        goHome() {
+            this.$router.push('/')
+        }
+    },
+}
+</script>
