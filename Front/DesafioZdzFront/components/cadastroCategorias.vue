232977d071d411fb38fa5cf7e68a2d9c42bb87c9
Glebson Santos <glebsonsantos@outlook.pt>
2024-06-03 11:06:04 -0300

    Fix bug visual do model e colocando a validacao

--- Front/DesafioZdzFront/components/cadastroCategorias.vue
+++ Front/DesafioZdzFront/components/cadastroCategorias.vue
@@ -3,19 +3,17 @@
     <v-dialog v-model="internalDialog" max-width="600">
       <v-card prepend-icon="mdi-ballot-outline" title="Adicionar Categoria">
         <v-card-text>
-          <form @submit.prevent="handleSubmit">
-            <v-text-field label="Nome" v-model="categoria.name"></v-text-field>
+          <form>
+            <v-text-field label="Nome *" v-model="categoria.name" error-messages="O nome deve ter 2 ou mais caracteres"></v-text-field>
             <v-select :items="items" label="Categoria Pai" density="comfortable" v-model="categoria.partentCategoryId"
-              item-value="id" item-title="name"></v-select>
+              item-value="id" item-title="name" clearable></v-select>
           </form>
         </v-card-text>
-
         <v-divider></v-divider>
-
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn text="Close" variant="plain" @click="closeDialog">FECHAR</v-btn>
-          <v-btn color="primary" text="Save" variant="tonal" @click="saveDialog" type="submit">SALVAR</v-btn>
+          <v-btn color="primary" text="Save" variant="tonal" @click="saveDialog" :disabled="!isValid()">SALVAR</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -23,26 +21,26 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, reactive, watch, onMounted } from 'vue'
 
-var categoria = {
+const categoria = reactive({
   id: null,
   name: "",
   partentCategoryId: ""
-};
+});
 
-/*var isValid = () => {
-      if (categoria.name == "") {
-        return false;
-      }
-      return true;
-};*/
+const isValid = () => {
+  if(categoria.name == "" || categoria.name.length < 2) {
+    return false;
+  }
+  return true;
+};
 
 const items = ref([]);
 
 const props = defineProps({ dialog: Boolean }) //Propriedade que vai vir do componente 
 
-const emit = defineEmits(['update:dialog'])
+const emit = defineEmits(['update:dialog']) //Emite o update pro componente pai
 
 const internalDialog = ref(props.dialog) //Variavel interna que vai receber o valor da propriedade e vai ser usada pra abrir/fechar o modal
 
@@ -80,6 +78,10 @@ const saveDialog = async () => { //Cadastrar
 
 watch(internalDialog, (newVal) => { //Watch para monitorar o valor da variavel interna e alterar a propriedade na pagina de categorias 
   emit('update:dialog', newVal)
+  if (newVal === false) {
+    categoria.name = "";
+    categoria.partentCategoryId = "";
+  }
 })
 
 const buscarCategorias = async () => { //Pegar as categorias pra selecionar a categoria pai na hora de adicionar
