803a13f35ebe568e1f31a33e94ecb5ae8d11a831
Glebson Santos <glebsonsantos@outlook.pt>
2024-06-04 20:32:57 -0300

    Listagem Produtos

--- Front/DesafioZdzFront/Pages/produtos/index.vue
+++ Front/DesafioZdzFront/Pages/produtos/index.vue
@@ -1,26 +1,65 @@
 <template>
-  <div class="d-flex align-md-center">
+  <div class="d-flex align-md-center mb-12">
     <h1 class="me-6">Produtos</h1>
     <v-icon color="green-darken-2" size="large" @click="dialog = !dialog"> mdi-plus-circle </v-icon>
-    <CadastroProdutos v-model:dialog="dialog"/>
+    <CadastroProdutos v-model:dialog="dialog" />
   </div>
+  <v-container>
+    <v-row dense>
+      <v-col cols="12" md="4" v-for="item in items">
+        <CardProduto :produto="item" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import CadastroProdutos from '~/components/cadastroProdutos.vue';
+import CadastroProdutos from '~/components/cadastroProdutos.vue'
+import CardProduto from '~/components/cardProduto.vue'
 
 export default {
   components: {
-    CadastroProdutos
+    CadastroProdutos,
+    CardProduto
   },
   data() {
     return {
-      dialog: false
+      dialog: false,
+      items: [],
+      categoria: Object
     }
+  },
+  methods: {
+    async buscarProdutos() {
+      try {
+        const response = await fetch('https://localhost:44350/api/products');
+        const data = await response.json();
+        this.items = await Promise.all(data.map(async (item) => {
+          item.categoryName = await this.buscarNameCategoriaById(item.categoryId);
+
+          return item;
+        }));
+      }
+      catch {
+        console.log('Deu ruim');
+      }
+    },
+    async buscarNameCategoriaById(id) {
+      try {
+        const response = await fetch('https://localhost:44350/api/categories/' + id);
+        const data = await response.json();
+        console.log(data.name)
+        return data.name;
+      }
+      catch {
+        console.log('Deu ruim');
+      }
+    }
+  },
+  mounted() {
+    this.buscarProdutos();
   }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ Front/DesafioZdzFront/components/cardProduto.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-sheet border="lg opacity-12" class="text-body-2 mx-auto" max-width="550">
+        <v-container fluid>
+            <v-row>
+                <v-col cols="12" md="3">
+                    <v-img height="120" :src="produto.imageUrl" cover aling="center"></v-img>
+                </v-col>
+
+                <v-col cols="12" md="9">
+                    <div class="mb-4 d-flex justify-space-between">
+                        <span>{{ produto.name }} -- {{ produto.categoryName }}</span>
+                        <v-icon icon="mdi-delete-circle" color="error" size="large"></v-icon>
+                    </div>
+                    <div>
+                        {{ produto.description }}
+                    </div>
+                    <div>
+                        Quantidade: {{ produto.quantity }}
+                    </div>
+                    <div>
+                        Preço: R$ {{ produto.price }}
+                    </div>
+                    <v-btn class="text-none mt-3" color="info" rounded="0" variant="flat" block text="EDITAR">
+                        
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-sheet>
+</template>
+
+<script setup>
+const prop = defineProps({
+    produto: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style></style>
